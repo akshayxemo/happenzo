96120516a486218178b4a96b8f0e217e4e697a8b
akshayxemo <akshaykrdas001@gmail.com>
2024-01-03 16:30:21 +0530

    Clark authentication system implemented

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,5 +1,7 @@
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "../ui/button";
 
 export const Header = () => {
   return (
@@ -8,13 +10,22 @@ export const Header = () => {
         <Link href={"/"} className="w-36">
           <Image
             src={"/assets/images/logo.svg"}
-            width={128}
-            height={38}
+            width={144}
+            height={54}
             alt="Happenzo Logo"
           />
         </Link>
 
-        <div className="w-32 flex justify-end gap-3"></div>
+        <div className="w-32 flex justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size={"lg"}>
+              <Link href={"/sign-in"}>Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
       </div>
     </header>
   );
